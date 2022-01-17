4adc15c748e2364ff2cb6448b94b875d34ffeb35
harshal <harshalaher07@gmail.com>
2022-01-17 19:19:46 +0530

    initial project setup

---
+++ blog-app-rest/src/main/java/com/springboot/blogapprest/BlogAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.blogapprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppRestApplication.class, args);
+	}
+
+}
---
+++ blog-app-rest/src/test/java/com/springboot/blogapprest/BlogAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.blogapprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
